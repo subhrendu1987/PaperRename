555367b0606766984426517c4f0251ddccd35d44
subhrendu@iitg.ernet.in <subhrendu@idrbt.ac.in>
2023-05-09 21:06:55 +0530

    modified:   RenamePDFs.py

--- RenamePDFs.py
+++ RenamePDFs.py
@@ -13,21 +13,42 @@ def parse_args():
     parser.add_argument('--acm', '-a',
                         action='store_true',
                         help="Article Type=ACM",
-                        default="False")
+                        default=False)
     parser.add_argument('--springer', '-s',
                         action='store_true',
                         help="Article Type=Springer",
-                        default="False")
+                        default=False)
     
     args = parser.parse_args()
     return args
 #############################################################################
+args=parse_args()
+#############################################################################
+def checkValid(title):
+	return(True)
+#############################################################################
+def renameFile(src,dst):
+	print(src,"--->",dst)
+#############################################################################
+def getTitle(page):
+	text = page.extract_text().split("\n")
+	text1=[t for i,t in enumerate(text) if(t.isprintable())]
+		print("[ %d ]"%i,t.isprintable(),t)
+#############################################################################
+''' STUB
+args.ieee=True
+args.acm=True
+args.springer=True
+'''
+#############################################################################
 for file in os.listdir(TARGET_DIR):
     if file.endswith(".pdf"):
-        print(os.path.join(TARGET_DIR, file))
-        reader = PdfReader(file)
+        PATH=os.path.join(TARGET_DIR, file)
+        reader = PdfReader(PATH)
         number_of_pages = len(reader.pages)
-        #page = reader.pages[0]
-        #text = page.extract_text()
-
+        print(PATH," having ",number_of_pages, " pages")
+        page = reader.pages[0]
+        title=getTitle(page)
+        if(checkValid(title)):
+        	renameFile(PATH,TARGET_DIR+"/"+title+".pdf")
 
